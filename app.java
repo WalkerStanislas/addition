f312fa5195397a78cdacee3cfbc4490dea2be894
walker <walkercompaore972@gmail.com>
2024-03-06 16:09:59 +0000

    code for addition

--- app.java
+++ app.java
@@ -1,5 +1,11 @@
 public class app {
-    public float addition(float a, float b) {
-        return (a + b);
+    public static void main(String[] args) {
+        addition(0, 0);
+    }
+
+    public static double addition(Number a, Number b) {
+        double a1 = a.doubleValue();
+        double b1 = b.doubleValue();
+        return a1 + b1;
     }
 }
