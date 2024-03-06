1d79cfbcce9c6ea7a47f9f21d6bfc45e83260d05
walker <walkercompaore972@gmail.com>
2024-03-06 16:16:04 +0000

    installations réquises

--- test.java
+++ test.java
@@ -1,4 +1,15 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class test {
-    
+
+    public static void main(String[] args) {
+        //
+    }
+
+    public static void zero_plus_0_test() {
+        app app_test = new app();
+        assertEquals(app_test.addition(0, 0), 0);
+    }
 }
